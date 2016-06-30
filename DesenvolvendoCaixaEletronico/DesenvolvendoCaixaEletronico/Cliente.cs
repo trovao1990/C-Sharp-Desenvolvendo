9a35b80d5e3dcd9debbccff2ded3a5c1d30f64aa
Vinicius Trovao Pereira <vini-trovao2@hotmail.com>
2016-06-30 11:31:01 -0300

    Criando classes Cliente e Conta

---
+++ DesenvolvendoCaixaEletronico/DesenvolvendoCaixaEletronico/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesenvolvendoCaixaEletronico
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ DesenvolvendoCaixaEletronico/DesenvolvendoCaixaEletronico/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesenvolvendoCaixaEletronico
+{
+    class Conta
+    {
+        public Cliente Titular { get; set; }
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+    }
+}
